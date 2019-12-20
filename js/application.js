124143862e2ea0c174ca308637c109a4a55d6084
andrew-ja-thomas <53881018+andrew-ja-thomas@users.noreply.github.com>
2019-12-20 15:56:25 -0700

    Add files via upload

--- js/application.js
+++ js/application.js
@@ -1,13 +1,22 @@
   tableau.extensions.initializeAsync().then(function () { 
-
     //code here:
-
-    tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "test").getSelectedMarksAsync().then(function (marks) {
-  
-      const worksheetData = marks.data[0].data[0][1]._value;
-      console.log(worksheetData);
-      $("#content").html(worksheetData)
-    });
-
-  //stop coding here
+    //define variables
+const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
+var worksheet = worksheets.find(function (sheet) {
+  return sheet.name === "Table";
+});
+const markSelection = tableau.TableauEventType.MarkSelectionChanged;
+// fetch htmls
+worksheet.addEventListener(markSelection, function (selectionEvent) {
+// When the selection changes, reload the data
+selectionEvent.getMarksAsync().then(
+  function (marks) {
+    const worksheetData = marks.data[0].data[0][3]._value;
+    const all_data = marks;
+    console.log(all_data);
+    $("#content").html(worksheetData);
+  })});
+      //stop coding here
 })
+
+
